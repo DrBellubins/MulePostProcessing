7603c3a1cfc0314fef494b83712a9334d176611e
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2025-01-12 17:14:04 -0500

    Lay groundwork for actual work :P

---
+++ MulePostProcessing/Scripts/MulePostProcessing.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class MulePostProcessing : Node
+{
+	[Export] public Material UberMaterial;
+
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
